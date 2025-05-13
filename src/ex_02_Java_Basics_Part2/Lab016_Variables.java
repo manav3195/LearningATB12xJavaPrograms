480af5338159400243782693636bf1d92fb79791
manav3195 <manavchaudhary3195@gmail.com>
2025-05-13 07:36:16 +0530

    13 may 2025

--- src/ex_02_Java_Basics_Part2/Lab016_Variables.java
+++ src/ex_02_Java_Basics_Part2/Lab016_Variables.java
@@ -3,7 +3,7 @@ package ex_02_Java_Basics_Part2;
 public class Lab016_Variables {
     public static void main(String[] args) {
         int a = 10;
-        a = 65+1;
+        a = 65 + 1;
         System.out.println(a);
     }
 }
