80a69b5ae964cbde7ed3a0eee51a1ca1775242b0
manav3195 <manavchaudhary3195@gmail.com>
2025-05-14 06:50:10 +0530

    13 may 2025

---
+++ src/ex_03_Literals/Boolean_Literals.java
@@ -0,0 +1,11 @@
+package ex_03_Literals;
+
+public class Boolean_Literals {
+
+    public static void main(String[] args)
+    {
+        boolean is_married = true;
+        boolean is_married_amit = false;
+
+    }
+}
