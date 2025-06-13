958f21c6bf39b383bbc6c12545efb009f103d322
manav3195 <manavchaudhary3195@gmail.com>
2025-06-13 07:40:41 +0530

    23 may 2025

---
+++ src/ex_05_TypeCasting/Lab057_TypeCasting_0.java
@@ -0,0 +1,10 @@
+package ex_05_TypeCasting;
+
+public class Lab057_TypeCasting_0 {
+    public static void main(String args[])
+    {
+        byte b=10;
+        int a =b; //implicit casting ,smaller data into bigger data type
+        System.out.println(a);
+    }
+}
---
+++ src/ex_05_TypeCasting/Lab058_TypeCasting.java
@@ -0,0 +1,12 @@
+package ex_05_TypeCasting;
+
+public class Lab058_TypeCasting {
+    public static void main(String args[])
+    {
+        byte b =10;
+        int a =b; // implicit typecasting
+        int a1 = (int)b; // explicit type casting
+        System.out.println(a1);
+
+    }
+}
