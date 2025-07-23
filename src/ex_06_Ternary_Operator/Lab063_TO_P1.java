3591ba2f0eaa45985aed507f2f2ec1a83290e0d1
manav3195 <manavchaudhary3195@gmail.com>
2025-07-23 07:30:25 +0530

    23 may 2025

--- src/ex_06_Ternary_Operator/Lab063_TO_P1.java
+++ src/ex_06_Ternary_Operator/Lab063_TO_P1.java
@@ -4,8 +4,8 @@ public class Lab063_TO_P1 {
 
     public static void main(String args[])
     {
-        int num = 5;
-        String result = (num>0) ? "positive":"negative";
+        int num1 = 5;
+        String result = (num1>0) ? "positive":"negative";
         System.out.println(result);
 
     }
