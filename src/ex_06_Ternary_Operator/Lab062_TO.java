a1504178344fb633c73d5090c27d61c3576aed9a
manav3195 <manavchaudhary3195@gmail.com>
2025-07-23 07:32:26 +0530

    23 may 2025

--- src/ex_06_Ternary_Operator/Lab062_TO.java
+++ src/ex_06_Ternary_Operator/Lab062_TO.java
@@ -4,8 +4,8 @@ public class Lab062_TO {
 
     public static void main(String args[])
     {
-        int age1=27;
-        String canIgoToGOA = (age1>18) ? "Yes you can" : "You can't" ;
+        int age=27;
+        String canIgoToGOA = (age>18) ? "Yes you can" : "You can't" ;
         System.out.println(canIgoToGOA);
     }
 }
--- src/ex_06_Ternary_Operator/Lab064_Interview_Ready_Question.java
+++ src/ex_06_Ternary_Operator/Lab064_Interview_Ready_Question.java
@@ -4,7 +4,7 @@ public class Lab064_Interview_Ready_Question {
     public static void main(String args[])
     {
         int age = 21;
-        String result1 = (age>18) ? (age>25 ? "In Goa you can drink" : "You can't drink")  :"You can't go to Goa";
-        System.out.println(result1);
+        String result = (age>18) ? (age>25 ? "In Goa you can drink" : "You can't drink")  :"You can't go to Goa";
+        System.out.println(result);
     }
 }
