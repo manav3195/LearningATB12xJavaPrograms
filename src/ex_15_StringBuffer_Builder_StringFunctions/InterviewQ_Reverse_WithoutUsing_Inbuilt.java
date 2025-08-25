ae99529b22acfd03c5544fa8ad027aa7df72a7bb
manav3195 <manavchaudhary3195@gmail.com>
2025-08-25 07:15:11 +0530

    23 may 2025

---
+++ src/ex_15_StringBuffer_Builder_StringFunctions/InterviewQ_Reverse_WithoutUsing_Inbuilt.java
@@ -0,0 +1,18 @@
+package ex_15_StringBuffer_Builder_StringFunctions;
+
+public class InterviewQ_Reverse_WithoutUsing_Inbuilt {
+    public static void main(String args[])
+    {
+        String name= "Pramod";
+        String reverse="";
+        for(int i = name.length()-1; i>=0; i--)
+        {
+            reverse= reverse + name.charAt(i);
+        }
+        System.out.println(reverse);
+
+        System.out.println("Using string buffer");
+        StringBuffer sb = new StringBuffer(name);
+        System.out.println(sb.reverse());
+    }
+}
