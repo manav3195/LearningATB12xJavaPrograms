7f1559967cc657f9169881c4b244b9e32c78a050
manav3195 <manavchaudhary3195@gmail.com>
2025-09-22 21:51:16 +0530

    23 may 2025

---
+++ src/ex_19_OOPs_Part2/inheritance/multipleinheritance/Lab172_HI.java
@@ -0,0 +1,10 @@
+package ex_19_OOPs_Part2.inheritance.multipleinheritance;
+
+public class Lab172_HI {
+
+    public static void main(String args[])
+    {
+        Son s1 = new Son();
+       // s1.money();
+    }
+}
